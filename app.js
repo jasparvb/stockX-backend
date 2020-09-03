1a2e384b9cb04ac854f97c385d18549ff838c2c9
von Buelow <jaspar@UMICH.EDU>
2020-09-02 22:00:39 -0400

    Server setup

---
+++ app.js
@@ -0,0 +1,44 @@
+/** Express app for stockX */
+
+
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// add logging system
+
+const morgan = require("morgan");
+app.use(morgan("tiny"));
+
+
+const usersRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+
+app.use("/users", usersRoutes);
+app.use("/", authRoutes);
+
+/** 404 handler */
+
+app.use(function (req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+
+  // pass the error to the next piece of middleware
+  return next(err);
+});
+
+/** general error handler */
+
+app.use(function (err, req, res, next) {
+  if (err.stack) console.log(err.stack);
+
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err,
+    message: err.message
+  });
+});
+
+
+module.exports = app;
