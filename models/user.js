88bfe107b5ec71b993e0717e87695ea745d8f90a
von Buelow <jaspar@UMICH.EDU>
2020-09-04 13:51:20 -0400

    Find, update, remove user

--- models/user.js
+++ models/user.js
@@ -68,5 +68,94 @@ class User {
     return result.rows[0];
   }
 
+  /** Find all users. */
 
+  static async findAll() {
+    const result = await db.query(
+        `SELECT username
+          FROM users
+          ORDER BY username`);
+
+    return result.rows;
+  }
+
+  /** Given a username, return data about user. */
+
+  static async findOne(username) {
+    const userRes = await db.query(
+        `SELECT username 
+            FROM users 
+            WHERE username = $1`,
+        [username]);
+
+    const user = userRes.rows[0];
+
+    if (!user) {
+      const error = new Error(`There exists no user '${username}'`);
+      error.status = 404;   // 404 NOT FOUND
+      throw error;
+    }
+
+    const userListsRes = await db.query(
+        `SELECT s.id, s.ticker, l.name 
+           FROM lists AS l
+             JOIN stocks AS s ON s.list_id = l.id
+           WHERE l.username = $1`,
+        [username]);
+
+    user.lists = userListsRes.rows;
+    return user;
+  }
+
+    /** Update user data with `data`.
+   *
+   * This is a "partial update" --- it's fine if data doesn't contain
+   * all the fields; this only changes provided ones.
+   *
+   * Return data for changed user.
+   *
+   */
+
+  static async update(username, data) {
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
+    }
+
+    let {query, values} = partialUpdate(
+        "users",
+        data,
+        "username",
+        username
+    );
+
+    const result = await db.query(query, values);
+    const user = result.rows[0];
+
+    if (!user) {
+      let notFound = new Error(`There exists no user '${username}`);
+      notFound.status = 404;
+      throw notFound;
+    }
+
+    delete user.password;
+
+    return result.rows[0];
+  }
+
+  /** Delete given user from database; returns undefined. */
+
+  static async remove(username) {
+      let result = await db.query(
+              `DELETE FROM users 
+                WHERE username = $1
+                RETURNING username`,
+              [username]);
+
+    if (result.rows.length === 0) {
+      let notFound = new Error(`There exists no user '${username}'`);
+      notFound.status = 404;
+      throw notFound;
+    }
+  }
+}
 module.exports = User;
