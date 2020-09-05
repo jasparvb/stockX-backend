c7e7e18d4634c085f9597bcca427e8387af9108a
von Buelow <jaspar@UMICH.EDU>
2020-09-05 19:41:42 -0400

    Api search fn

--- helpers/iexCloudApi.js
+++ helpers/iexCloudApi.js
@@ -1,13 +1,15 @@
 /*API calls to iexcloud.io */
-import APIKEY from './Apikey'
+const axios = require("axios");
+const APIKEY = require("../Apikey");
 
 const BASE_URL = "https://sandbox.iexapis.com/stable"
 
 class IexCloudApi {
   static async search(term) {
-    const result = await axios.get(`${BASE_URL}/search/${term}`, {token: APIKEY});
-    console.log(result);
-    return result;
+    const result = await axios.get(`${BASE_URL}/search/${term}`, {
+      params: {token: APIKEY}
+    });
+    return result.data;
   }
 }
 
